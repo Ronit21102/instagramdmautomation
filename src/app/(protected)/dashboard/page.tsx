f8c4981d5620ff375a283b68eb8d8d42f2cbdd6b
ronit21102-js <ronit21102@gmail.com>
2024-12-31 09:31:28 +0530

    pisma setup

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Page = async (props: Props) => {
+
+    // server action onboard the user
+    // then we can redirect to the dashboard to the particular user id dashboard
+
+  return <div>Page</div>;
+};
+
+export default Page;
