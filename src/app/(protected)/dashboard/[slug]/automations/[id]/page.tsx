b9cfdf7532eb189a6337917a57e2180a803edcde
ronit21102-js <ronit.roushan@kreedalabs.com>
2025-01-01 16:09:27 +0530

    frontend for automation is done

--- src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
+++ src/app/(protected)/dashboard/[slug]/automations/[id]/page.tsx
@@ -1,18 +1,27 @@
-import AutomationsBreadCrumb from '@/components/global/bread-crumbs/automations'
-import React from 'react'
+import Trigger from "@/components/global/automations/Trigger";
+import AutomationsBreadCrumb from "@/components/global/bread-crumbs/automations";
+import { Warning } from "@/icons";
+import React from "react";
 
 type Props = {
-    params:{id:string}
-}
+  params: { id: string };
+};
 
 //WIP set some metaDta
-const Page = ({params}: Props) => {
-    //WIP prefetch user automation data
+const Page = ({ params }: Props) => {
+  //WIP prefetch user automation data
   return (
-    <div className='flex flex-col items-center gap-y-20'>
-        <AutomationsBreadCrumb />
+    <div className="flex flex-col items-center gap-y-20">
+      <AutomationsBreadCrumb id={params.id} />
+      <div className="w-full lg:w-10/12 xl:w-6/12 p-5 rounded-xl flex flex-col bg-[#1D1D1D] gap-y-3">
+        <div className="flex gap-x-2">
+          <Warning />
+          When...
+        </div>
+        <Trigger id={params.id} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
